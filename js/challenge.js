b783930b4eb6bb8b8e812c77893bea47609ee486
Kyle <kylewilkinson14@gmail.com>
2021-11-13 17:53:59 +1100

    likes working

--- js/challenge.js
+++ js/challenge.js
@@ -7,13 +7,14 @@ const likeList = document.querySelector(".likes")
 const form = document.querySelector("#comment-form")
 const commentInput = document.querySelector("#comment-input")
 const commentsList = document.querySelector("#list")
-const likes = {
+const likes = {}
 
-}
 let timer = setInterval(incrementCount, 1000)
+
 plusButton.addEventListener("click", incrementCount)
 minusButton.addEventListener("click", decrementCount)
 pauseButton.addEventListener("click", toggleCount)
+likeButton.addEventListener("click", increaseLike)
 
 function incrementCount() {
     let counterValue = parseInt(counter.innerText)
@@ -34,5 +35,25 @@ function toggleCount(event) {
     } else {
         timer = setInterval(incrementCount, 1000)
         event.target.innerText = "pause"
+    }
+}
+
+function increaseLike() {
+    const key = counter.innerText
+    
+    if (likes[key]) likes[key]++
+    else likes[key] = 1
+
+    for (const key in likes) {
+        let currentItem = document.querySelector(`#_${key}`)
+        if (currentItem) {
+            currentItem.innerText = `${key} has been liked ${likes[key]} times`
+        } else {
+            currentItem = document.createElement("li")
+            currentItem.id = `_${key}`
+
+            currentItem.innerText = `${key} has been liked ${likes[key]} times`
+            likeList.append(currentItem)
+        }
     }
 }
